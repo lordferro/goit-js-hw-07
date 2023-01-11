dbb370aae262edf73a9f28be8dd056d3cf0c29db
Fedor Romanovschi <110813692+lordferro@users.noreply.github.com>
2023-01-11 18:13:10 +0100

    Done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,72 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef =
+  document.querySelector(".gallery");
+
+const galleryItemsMarkup =
+  makeGalleryMarkup(galleryItems);
+let originalLink = null;
+
+//   Insert markup in DOM
+galleryRef.insertAdjacentHTML(
+  "afterbegin",
+  galleryItemsMarkup
+);
+
+// Listener
+galleryRef.addEventListener(
+  "click",
+  onGalleryItemClick
+);
+
+// function to make gallery markup
+function makeGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+// delegetion
+function onGalleryItemClick(evt) {
+  evt.preventDefault();
+
+  if (
+    !evt.target.classList.contains(
+      "gallery__image"
+    )
+  ) {
+    return;
+  }
+
+  originalLink = evt.target.dataset.source;
+
+  const modal = basicLightbox.create(
+    `<img width="1400" height="900" src="${originalLink}">`,
+    {
+      onShow: () => {
+        document.addEventListener(
+          "keydown",
+          (evt) => {
+            if (evt.code === "Escape") {
+              modal.close();
+            }
+          }
+        );
+      },
+    }
+  );
+  modal.show();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+const listItemsMarkup =
+    makeListMarkup(galleryItems);
+  
+listEl.innerHTML = listItemsMarkup;
+
+function makeListMarkup(items) {
+  return items
+    .map((item) => {
+      return `<a class="gallery__item" href="${item.original}">
+            <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+            </a>`;
+    })
+    .join("");
+}
+
+const imgEl = document.querySelector('.gallery__image');
+console.log(imgEl);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
